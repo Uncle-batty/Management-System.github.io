e08598173a11c92056e68a618450a380ecd74273
Louis batty <93161778+Uncle-batty@users.noreply.github.com>
2024-06-14 12:19:27 +0200

    Added Google auth

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -21,6 +21,18 @@ export const Login = () => {
         }
     }
 
+    async function signInWithOAuth() {
+        let { data, error } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+        });
+
+        if (error) {
+            console.error('Error with OAuth sign-in:', error.message);
+        } else {
+            console.log('OAuth sign-in successful:', data);
+        }
+    }
+
     return (
         <div className='main'>
             <div className='container'>
@@ -39,7 +51,7 @@ export const Login = () => {
                 <div className="social-account-container">
                     <span className="title">Or Sign in with</span>
                     <div className="social-accounts">
-                        <button className="social-button google">
+                        <button className="social-button google" onClick={signInWithOAuth}>
                             <svg className="svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 488 512">
                                 <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
                             </svg>
